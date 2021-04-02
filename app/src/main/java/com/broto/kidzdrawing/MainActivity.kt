e87f9878b63d9e4ba9efb57e7f64e716f0bcb4e3
Anibrata Saha <broto76@gmail.com>
2021-04-02 13:12:45 +0530

    Added UI and logic for setting a custom background from the Gallery. Also added the necessary permission related stuff.
    Signed-off-by: Anibrata Saha <broto76@gmail.com>

--- app/src/main/java/com/broto/kidzdrawing/MainActivity.kt
+++ app/src/main/java/com/broto/kidzdrawing/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.broto.kidzdrawing
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +25,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         drawing_view.setSizeForBrush(22.toFloat())
 
@@ -30,6 +37,38 @@ class MainActivity : AppCompatActivity() {
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
         }
+
+        ib_gallery.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhotoIntent, GALLERY)
+            } else {
+                requestStoragePermission()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == GALLERY) {
+            try {
+                if(data?.data != null) {
+                    iv_background.visibility = View.VISIBLE
+                    iv_background.setImageURI(data.data)
+                } else {
+                    Toast.makeText(this, "Unable to set background",
+                        Toast.LENGTH_SHORT).show()
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 
     private fun showBrushSizeChooserDialog() {
@@ -71,5 +110,45 @@ class MainActivity : AppCompatActivity() {
             )
             mImageButtonCurrentPaint = imageButton
         }
+    }
+
+    private fun requestStoragePermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())) {
+            Toast.makeText(this, "Need Permission to add Background",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission for storage Granted",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Cannot set custom background without permission",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 }
